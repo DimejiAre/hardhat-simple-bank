db1c377f12f400ed0b0550bc772fb7f44b7737eb
DimejiAre <olardi2@gmail.com>
2023-04-27 09:15:47 +0100

    feat: add tests for deposit and withdrawal

--- test/unit/simple-bank-test.js
+++ test/unit/simple-bank-test.js
@@ -1,4 +1,4 @@
-const { deployments, getNamedAccounts, network } = require('hardhat')
+const { deployments, getNamedAccounts, network, ethers } = require('hardhat')
 const { assert } = require('chai')
 const { networkConfig } = require('../../helper-hardhat-config')
 
@@ -15,6 +15,40 @@ describe("SimpleBank", function () {
             const expectedMinimumDepositUsd = (networkConfig[network.config.chainId]['minimumDepositUsd'] * 1e18).toString()
             const miminimumDepositUsd = (await simpleBank.getMinimumDepositUsd()).toString()
             assert.equal(miminimumDepositUsd, expectedMinimumDepositUsd)
+        })
+    })
+
+    describe("deposit", function () {
+        let depositValue
+        beforeEach(async function(){
+            depositValue = ethers.utils.parseEther("0.1")
+            const tx = await simpleBank.deposit({value: depositValue})
+            await tx.wait(1)
+        })
+        it("saves the depositors address", async function () {
+            const depositor = await simpleBank.getDepositor(0)
+            assert.equal(depositor, deployer)
+        })
+        it("adds deposit value to the depositors balance", async function () {
+            const depositorsBalance = await simpleBank.getDepositorBalance(deployer)
+            assert.equal(depositorsBalance.toString(), depositValue.toString())
+        })
+    })
+
+    describe("withdraw", function () {
+        let depositValue, withdrawValue
+        beforeEach(async function(){
+            depositValue = ethers.utils.parseEther("0.5")
+            const depositTx = await simpleBank.deposit({value: depositValue})
+            await depositTx.wait(1)
+            withdrawValue = ethers.utils.parseEther("0.2")
+            const withdrawTx = await simpleBank.withdraw(withdrawValue)
+            await withdrawTx.wait(1)
+        })
+        it("debits the depositors balance", async function () {
+            const depositorsBalance = await simpleBank.getDepositorBalance(deployer)
+            const balance = depositValue.sub(withdrawValue)
+            assert.equal(depositorsBalance.toString(), balance.toString())
         })
     })
 })
