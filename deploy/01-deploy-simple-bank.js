8653073d5ad7a729d745dc37dbe7eb080dcff901
DimejiAre <olardi2@gmail.com>
2023-04-27 12:31:54 +0100

    feat: update simpleBank deploy script to deploy mocks on development chains

--- deploy/01-deploy-simple-bank.js
+++ deploy/01-deploy-simple-bank.js
@@ -1,16 +1,25 @@
 const { network } = require('hardhat')
-const { networkConfig } = require('../helper-hardhat-config');
+const { networkConfig, developmentChains } = require('../helper-hardhat-config');
 
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
     const minimumDepositUsd = networkConfig[network.config.chainId]['minimumDepositUsd']
-    const args = [minimumDepositUsd]
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const mockV3aggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = mockV3aggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[network.config.chainId]['ethUsdPriceFeedAddress']
+    }
+    const args = [minimumDepositUsd, ethUsdPriceFeedAddress]
     await deploy('SimpleBank', {
         from: deployer,
         args,
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1
     });
 };
 module.exports.tags = ['all', 'simpleBank'];
